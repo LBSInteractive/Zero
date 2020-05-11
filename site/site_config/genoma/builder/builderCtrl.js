6ff5e05b4ea2c1d32904b1d939bf69f94270cf23
Steve <wezarkevo@gmail.com>
2020-05-11 15:52:56 -0500

    minimal changes
    Minimal

--- site/site_config/genoma/builder/builderCtrl.js
+++ site/site_config/genoma/builder/builderCtrl.js
@@ -14,6 +14,12 @@ var core = angular.module('core', []);
 core.controller('control', function ($scope, $timeout, $rootScope, $http) {
 
 
+    $scope.miVar = "Hola";
+    console.log($scope.miVar);
+
+
+
+
     //****************************************************************************
     /*---------------------------- scope controller ----------------------------*/
     $scope.dataBase = {
@@ -51,6 +57,19 @@ core.controller('control', function ($scope, $timeout, $rootScope, $http) {
     $scope.$gui = {
         testDB: function () {
             $scope.dataBase.execution.testDB = true;
+        },
+        callService: () => {
+            var req = {
+                method: 'GET',
+                url: 'https://localhost/site/site_config/genoma/builder/builder.html'
+            }
+
+            $http(req).then(function (data) {
+                console.log(data);
+            }, function (data) {
+                console.log(data);
+
+            });
         }
     };
     //****************************************************************************
@@ -61,7 +80,22 @@ core.controller('control', function ($scope, $timeout, $rootScope, $http) {
     /*----------------------------     Watchers     ----------------------------*/
     $scope.$watchGroup(['dataBase.mySQL.disabled', 'dataBase.oracle.disabled', 'dataBase.host.val', 'dataBase.dbName.val', 'dataBase.user.val', 'dataBase.password.val'], function (newValue, oldValue, scope) {
         $scope.dataBase.dataBaseColor.power = ($scope.dataBase.mySQL.disabled && $scope.dataBase.host.val != '' && $scope.dataBase.dbName.val != '' && $scope.dataBase.user.val != '' && $scope.dataBase.password.val != '') ? true : false;
+
+        var req = {
+            method: 'POST',
+            url: 'http://example.com',
+            headers: {
+                'Content-Type': undefined
+            },
+            data: { host: $scope.dataBase.host.val }
+        }
+
+
+        console.log(req.data.host);
     });
+
+
+
 
     //****************************************************************************
 
